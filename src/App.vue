bc58c7408e349ba205e79f1e4ee208d12b0f6b97
Jeg0r4iks <ja00020@rvt.lv>
2025-01-20 22:32:57 +0200

    added new views to navbar

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1 @click="goHome">Mireqx</h1>
+
+    <nav>
+      <router-link to="/history">History</router-link>
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  methods: {
+    goHome() {
+      this.$router.push("/home"); 
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: blanchedalmond;
+}
+
+h1 {
+  color: black;
+  font-size: 65px;
+  font-family: Helvetica;
+  text-align: center;
+}
+
+nav {
+  display: flex;
+  justify-content: center;
+  gap: 15px;
+  margin-bottom: 20px;
+  padding: 10px;
+  color: black;
+}
+
+a {
+  font-size: 20px;
+  text-decoration: none;
+  color: black;
+  font-family: Helvetica;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
---
+++ src/views/HistoryView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>History of Fashion</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HistoryView',
+};
+</script>
+
+<style scoped>
+p {
+  color: black;
+  font-size: 30px;
+  font-family: Helvetica;
+}
+</style>
