175751afb2337a069897bce286ad9a6a22d5e9d8
Jeg0r4iks <ja00020@rvt.lv>
2025-01-21 14:56:19 +0200

    updated historyview

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,12 +1,41 @@
 <template>
   <div>
     <h1>History of Fashion</h1>
+    <p>Explore different designers</p>
+
+    <ul class="brand-list">
+      <li v-for="brand in filteredBrands" 
+      :key="brand.id"
+      class="brand-item"
+      >
+        {{ brand.name }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HistoryView',
+  name: 'HistoryView', 
+  data() {
+    return {
+      brands: [
+        { id: 0, name: 'All'},
+        { id: 1, name: 'Alexander Mcqueen' },
+        { id: 2, name: 'Balenciaga' },
+        { id: 3, name: 'Comme des Garcons' },
+        { id: 4, name: 'Dsquared' }
+      ],
+      filter: ''
+    };
+  },
+  computed: {
+    filteredBrands() {
+      return this.brands.filter((brand) =>
+        brand.name.toLowerCase().includes(this.filter.toLowerCase())
+      );
+    }
+  }
 };
 </script>
 
@@ -16,5 +45,36 @@ h1 {
   color: black;
   font-size: 35px;
   font-family: 'Gill Sans';
+}
+
+p { 
+  text-align: center;
+  color: black;
+  margin-top: 45px;
+  font-size: 25px;
+  font-family: 'Gill Sans';
+  font-weight: normal;
+}
+
+.brand-list {
+  justify-content: center;
+  gap: 10px;
+  text-decoration: none;
+  display: flex;
+  list-style: none;
+  font-family: Arial;
+  font-weight: bold;
+  color: black; 
+  padding: 5px 10px; 
+}
+
+.brand-item {
+  text-decoration: none; 
+  font-family: Arial;
+  color: black; 
+  padding: 5px 10px; 
+  border: 1px solid black; 
+  border-radius: 25px;
+  background-color: blanchedalmond; 
 }
 </style>
