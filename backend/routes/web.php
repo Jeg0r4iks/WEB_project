7f139d3ac057199a93f394700e4e4a50e1d80306
Jeg0r4iks <ja00020@rvt.lv>
2025-02-18 14:28:29 +0200

    backend deleted

--- backend/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\FormController;
-
-Route::get('/form', [FormController::class, 'showForm']);
-Route::post('/form-submit', [FormController::class, 'handleForm'])->name('form.submit');
-
