8cebef9e2e1a202ba24cd5c7059a6dad7bef0d60
Jeg0r4iks <ja00020@rvt.lv>
2025-02-18 10:44:15 +0200

    backend

---
+++ backend/app/Http/Controllers/FormController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\FormData;
+
+class FormController extends Controller
+{
+    public function showForm()
+    {
+        return view('form');
+    }
+
+    public function handleForm(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'message' => 'required|string'
+        ]);
+
+        FormData::create($request->all());
+
+        return redirect('/form')->with('success', 'Форма отправлена!');
+    }
+}
